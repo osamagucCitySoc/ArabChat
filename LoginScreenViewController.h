8f2d6776fe16a85980ee6512f39350ea9dd90bda
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2015-03-08 09:45:59 +0400

    1- Added the login page UI. 2- Related the login ui components to the login controller.

---
+++ LoginScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginScreenViewController.h
+//  ArabChat
+//
+//  Created by Osama Rabie on 3/8/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginScreenViewController : UIViewController
+
+@end
---
+++ LoginScreenViewController.m
@@ -0,0 +1,31 @@
+//
+//  LoginScreenViewController.m
+//  ArabChat
+//
+//  Created by Osama Rabie on 3/8/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import "LoginScreenViewController.h"
+
+@interface LoginScreenViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LoginScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)loginClicked:(id)sender {
+    
+}
+@end
